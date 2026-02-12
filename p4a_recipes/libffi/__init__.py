d7439a608dc842d837b929af2c4dd95d6cf656cf
chenyh <twr_chain@qq.com>
2026-02-12 15:32:49 +0800

    Fix p4a libffi local recipe export

--- p4a_recipes/libffi/__init__.py
+++ p4a_recipes/libffi/__init__.py
@@ -5,3 +5,5 @@ class LibffiRecipe(BaseLibffiRecipe):
     # Upstream tag names include the leading 'v', e.g. v3.4.4, so override the URL
     url = "https://github.com/libffi/libffi/archive/refs/tags/v{version}.tar.gz"
 
+
+recipe = LibffiRecipe()
