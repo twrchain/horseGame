daaac9946d53722a360c8f3a9a9da3a54bc1bb1b
chenyh <twr_chain@qq.com>
2026-02-12 15:38:19 +0800

    Skip libffi recipe patches when using local URL

--- p4a_recipes/libffi/__init__.py
+++ p4a_recipes/libffi/__init__.py
@@ -4,6 +4,7 @@ from pythonforandroid.recipes.libffi import LibffiRecipe as BaseLibffiRecipe
 class LibffiRecipe(BaseLibffiRecipe):
     # Upstream tag names include the leading 'v', e.g. v3.4.4, so override the URL
     url = "https://github.com/libffi/libffi/archive/refs/tags/v{version}.tar.gz"
+    patches = []
 
 
 recipe = LibffiRecipe()
